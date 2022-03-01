879841a3d3dd27bbde492d4fcf06e167e89d4c1d
Micky Kim <miqueaskimhan@gmail.com>
2022-03-01 14:49:05 -0600

    Fixed carousel

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -1,120 +1,99 @@
-import React, { useEffect, useState } from "react";
-import Glide, { Options } from "@glidejs/glide";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import SwiperCore, { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import required modules
+import "swiper/css";
+import "swiper/css/pagination";
 import modelos1 from "../../public/img/modelos1.png";
 import modelos2 from "../../public/img/modelos2.png";
 import modelos3 from "../../public/img/modelos3.png";
 import modelos4 from "../../public/img/modelos4.png";
 import Image from "next/image";
 
-const sliderConfig: Options = {
-    gap: 20,
-    perView: 4,
-    type: "carousel",
-    startAt: 2,
-    breakpoints: {
-        480: {
-            perView: 1,
-        },
-        800: {
-            perView: 2,
-            focusAt: 1,
-        },
-    },
-    focusAt: 2,
-};
-
-function Carousel(): JSX.Element {
-    const slider = new Glide(".glide", sliderConfig);
-
-    useEffect(() => {
-        slider.mount();
-    }, [slider]);
-
+export default function App() {
+    SwiperCore.use([Pagination]);
     return (
-        <>
-            <div className="glide bg-light-gray">
-                <h2 className="m-16 text-center text-2xl font-bold tracking-widest text-black">
-                    MODELOS
-                </h2>
-                <div className="glide__track m-8 h-80" data-glide-el="track">
-                    <ul className="glide__slides text-center text-lg">
-                        <li className="glide__slide slider ">
-                            <Image
-                                src={modelos1}
-                                alt="modelos1"
-                                height="250"
-                                width="250"
-                            />
-                            <p>Lorem Ipsum</p>
-                        </li>
-                        <li className="glide__slide slider">
-                            <Image
-                                src={modelos2}
-                                alt="modelos2"
-                                height="250"
-                                width="250"
-                            />
-                            <p>Lorem Ipsum</p>
-                        </li>
-                        <li className="glide__slide slider">
-                            <Image
-                                src={modelos3}
-                                alt="modelos3"
-                                height="250"
-                                width="250"
-                            />
-                            <p>Lorem Ipsum</p>
-                        </li>
-                        <li className="glide__slide slider">
-                            <Image
-                                src={modelos4}
-                                alt="modelos4"
-                                height="250"
-                                width="250"
-                            />
-                            <p>Lorem Ipsum</p>
-                        </li>
-                        <li className="glide__slide slider ">
-                            <Image
-                                src={modelos1}
-                                alt="modelos1"
-                                height="250"
-                                width="250"
-                            />
-                            <p>Lorem Ipsum</p>
-                        </li>
-                        <li className="glide__slide slider">
-                            <Image
-                                src={modelos2}
-                                alt="modelos2"
-                                height="250"
-                                width="250"
-                            />
-                            <p>Lorem Ipsum</p>
-                        </li>
-                        <li className="glide__slide slider">
-                            <Image
-                                src={modelos3}
-                                alt="modelos3"
-                                height="250"
-                                width="250"
-                            />
-                            <p>Lorem Ipsum</p>
-                        </li>
-                        <li className="glide__slide slider">
-                            <Image
-                                src={modelos4}
-                                alt="modelos4"
-                                height="250"
-                                width="250"
-                            />
-                            <p>Lorem Ipsum</p>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </>
+        <div className="bg-light-gray p-8 text-center">
+            <h2 className=" p-8 text-2xl font-bold text-black">MODELOS</h2>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                breakpoints={{
+                    768: {
+                        slidesPerView: 2,
+                    },
+                    1100: {
+                        slidesPerView: 4,
+                    },
+                }}
+                pagination={{
+                    el: ".my-custom-pagination-div",
+                    clickable: true,
+                    renderBullet: (index, className) => {
+                        return '<span class="' + className + '">' + "</span>";
+                    },
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <Image
+                        src={modelos1}
+                        alt="modelos1"
+                        height="250"
+                        width="250"
+                    />
+                    <p>Lorem Ipsum</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image
+                        src={modelos2}
+                        alt="modelos2"
+                        height="250"
+                        width="250"
+                    />
+                    <p>Lorem Ipsum</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image
+                        src={modelos3}
+                        alt="modelos3"
+                        height="250"
+                        width="250"
+                    />
+                    <p>Lorem Ipsum</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image
+                        src={modelos4}
+                        alt="modelos4"
+                        height="250"
+                        width="250"
+                    />
+                    <p>Lorem Ipsum</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image
+                        src={modelos1}
+                        alt="modelos1"
+                        height="250"
+                        width="250"
+                    />
+                    <p>Lorem Ipsum</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image
+                        src={modelos2}
+                        alt="modelos2"
+                        height="250"
+                        width="250"
+                    />
+                    <p>Lorem Ipsum</p>
+                </SwiperSlide>
+            </Swiper>
+            <div className="my-custom-pagination-div "></div>
+        </div>
     );
 }
-
-export default Carousel;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
 import "../styles/globals.css";
-import "swiper/css";
-import "swiper/css/pagination";
 
 import "../node_modules/@glidejs/glide/src/assets/sass/glide.core.scss";
 import "../node_modules/@glidejs/glide/src/assets/sass/glide.theme.scss";
